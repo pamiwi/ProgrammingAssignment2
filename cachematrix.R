76cbf59161fca99e8e49d56b9d34093aebd3ed97
Patrick Williams <patrick.williams.us@gmail.com>
2017-08-04 12:33:27 -0400

    MakeCacheMatrix and cacheSolve functions
    Wrote code for MakeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Taken together, makeCachematrix and cachemean create
+## a streamlined process for creating a square matrix and
+## then inteverting it
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function which sets and retrieves
+## the inverse of the Matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix() {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) s <<- solve
+        getSolve <- function() s
+        list(set = set, get = get, 
+             setSolve = setSolve, 
+             getSolve = getSolve)
+        }
 
-}
 
-
-## Write a short comment describing this function
+## cacheSolve takes the setSolve and getSolve values
+## created from the makeCacheMatrix and then inverts
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       s <- x$setSolve()
+       if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+       }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setSolve(x)
+        s
 }
